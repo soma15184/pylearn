160a0d2d9deba10acd55a23c556d71e75d3a5c41
somesh <soma15184@gmail.com>
2018-01-07 07:58:46 +0000

    ex1.py commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#!/bin/python
+first_name="someshwar"
+last_name="naiknavare"
+age=34
+birth_date="11/02/1984"
+print("my name is %s %s " % (first_name,last_name))
+print("i was born on %s and i am %d years old" % (birth_date,age))
